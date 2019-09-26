cf02ea1bd23dad203077dc725ca4d35b822c49be
simon3000 <simon3000@163.com>
2019-09-26 14:48:05 +0200

    runtime/version/platform[/name]

--- ws.js
+++ ws.js
@@ -12,10 +12,14 @@ const parse = string => {
 
 const wss = new WebSocket.Server({ port: 9013 })
 
+const url = new URL('https://cluster.vtbs.moe')
+const metadatas = ['runtime', 'platform', 'version', 'name']
+
 console.log('ws: 9013')
 
 module.exports = httpHome => {
-  wss.on('connection', ws => {
+  wss.on('connection', (ws, request) => {
+    const { searchParams } = new URL(request.url, url)
     const resolveTable = {}
     const uuid = httpHome.join(url => {
       console.log(`dispatch to ${uuid}`)
@@ -31,7 +35,15 @@ module.exports = httpHome => {
         resolveTable[key] = resolve
       })
     })
+
+    metadatas
+      .map(key => [key, searchParams.get(key)])
+      .filter(([_k, v]) => v)
+      // eslint-disable-next-line no-return-assign
+      .forEach(([k, v]) => httpHome.homes.get(uuid)[k] = v)
+
     console.log(`cluster connect ${uuid}`)
+
     ws.on('message', message => {
       if (message === 'DDhttp') {
         console.log(`pull ${uuid}`)
