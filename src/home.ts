48a686be92f2dbba116320d5efdd75ad2e9ed662
simon3000 <simon30002021@icloud.com>
2020-01-26 01:20:01 +0100

    噔噔咚 reverse

---
+++ src/home.ts
@@ -0,0 +1,83 @@
+import SocketIO from 'socket.io'
+import LRU from 'lru-cache'
+import AtHome from 'athome'
+import CState from '../state-center/api'
+import { map } from './metadata'
+
+export const cState = new CState({ name: 'cluster' })
+
+const io = SocketIO(9012, { serveClient: false })
+
+export const httpHome = new AtHome({
+  retries: 16,
+  validator: result => {
+    if (!result) {
+      return false
+    }
+    if (result.code) {
+      cState.log('error with code', { result })
+    }
+    if (result.data === undefined) {
+      cState.log('error unknow result', { result })
+      return false
+    }
+    return !result.code
+  },
+})
+
+const cache = new LRU({ max: 10000, maxAge: 1000 * 60 })
+
+cState.stateRoute({
+  pulls() {
+    return httpHome.pulls.length
+  },
+  pending() {
+    return httpHome.pending.length
+  },
+  homes() {
+    return [...httpHome.homes.values()]
+      .map(home => {
+        const { resolves, rejects, lastSeen, id } = home
+        const metadata = map.get(home)
+        return { resolves, rejects, lastSeen, id, ...metadata }
+      })
+  },
+  online() {
+    return httpHome.homes.size
+  },
+})
+
+const pending = new Map()
+
+io.on('connect', socket => {
+  cState.log('vtbs.moe connected')
+  socket.on('http', async (url, ack) => {
+    if (typeof ack === 'function') {
+      let result = cache.get(url)
+      if (result) {
+        cState.log('cached', { url })
+      } else if (pending.has(url)) {
+        result = await pending.get(url)
+      } else {
+        cState.log('executing', { url })
+        const time = Date.now()
+        const job = httpHome.execute(url).catch(e => {
+          cState.log('error', { msg: e.message || e })
+          console.error(e.message || e)
+          return undefined
+        })
+        pending.set(url, job)
+        result = await job
+        pending.delete(url)
+        if (result) {
+          cState.log('complete', { url, time: Date.now() - time })
+          cache.set(url, result)
+        }
+      }
+      ack(result)
+    }
+  })
+})
+
+console.log('Cluster center online')
+console.log('socket.io: 9012')
--- src/ws.ts
+++ src/ws.ts
@@ -2,6 +2,7 @@ import { Server } from 'ws'
 import AtHome from 'athome'
 import CState from '../state-center/api'
 import { map, metadatas } from './metadata'
+import { httpHome, cState } from './home'
 
 const keyGen = () => String(Math.random())
 const parse = (string: string) => {
@@ -27,97 +28,98 @@ const url = new URL('https://cluster.vtbs.moe')
 
 console.log('ws: 9013')
 
-export default (httpHome: InstanceType<typeof AtHome>, log: CState['log']) => {
-  wss.on('connection', (ws, request) => {
-    const resolveTable = new Map<string, (data: any) => void>()
-    const uuid = httpHome.join((url: string) => {
-      log('dispatch', { uuid })
-      const key = keyGen()
-      ws.send(JSON.stringify({
-        key,
-        data: {
-          type: 'http',
-          url,
-        },
-      }))
-      return new Promise((resolve, reject) => {
-        const timeout = setTimeout(reject, 1000 * 15, 'timeout')
-        resolveTable.set(key, data => {
-          clearTimeout(timeout)
-          resolve(data)
-        })
+const { log } = cState
+
+wss.on('connection', (ws, request) => {
+  const resolveTable = new Map<string, (data: any) => void>()
+  const uuid = httpHome.join((url: string) => {
+    log('dispatch', { uuid })
+    const key = keyGen()
+    ws.send(JSON.stringify({
+      key,
+      data: {
+        type: 'http',
+        url,
+      },
+    }))
+    return new Promise((resolve, reject) => {
+      const timeout = setTimeout(reject, 1000 * 15, 'timeout')
+      resolveTable.set(key, data => {
+        clearTimeout(timeout)
+        resolve(data)
       })
     })
+  })
 
-    console.log('online:', httpHome.homes.size)
+  console.log('online:', httpHome.homes.size)
 
-    const { searchParams } = new URL(request.url, url)
-    metadatas
-    map.set(httpHome.homes.get(uuid), Object.fromEntries(metadatas
-      .map(key => [key, searchParams.get(key)])
-      .filter(([_, v]) => v)))
+  const { searchParams } = new URL(request.url, url)
+  metadatas
+  map.set(httpHome.homes.get(uuid), Object.fromEntries(metadatas
+    .map(key => [key, searchParams.get(key)])
+    .filter(([_, v]) => v)))
 
-    log('connect', { uuid })
+  log('connect', { uuid })
 
-    const router = {
-      pulls() {
-        return httpHome.pulls.length
-      },
-      pending() {
-        return httpHome.pending.length
-      },
-      homes() {
-        return [...httpHome.homes.values()]
-          .map(home => {
-            const { resolves, rejects, lastSeen, id } = home
-            const metadata = map.get(home)
-            return { resolves, rejects, lastSeen, id, ...metadata }
-          })
-      },
-      online() {
-        return httpHome.homes.size
-      },
-    }
+  const router = {
+    pulls() {
+      return httpHome.pulls.length
+    },
+    pending() {
+      return httpHome.pending.length
+    },
+    homes() {
+      return [...httpHome.homes.values()]
+        .map(home => {
+          const { resolves, rejects, lastSeen, id } = home
+          const metadata = map.get(home)
+          return { resolves, rejects, lastSeen, id, ...metadata }
+        })
+    },
+    online() {
+      return httpHome.homes.size
+    },
+  }
 
-    ws.on('message', (message: string) => {
-      if (message === 'DDDhttp') {
-        if (httpHome.pending.length) {
-          log('pull', { uuid })
-          httpHome.pull(uuid)
-        } else {
-          ws.send('wait')
-        }
-      } else if (message === 'DDhttp') {
+  ws.on('message', (message: string) => {
+    if (message === 'DDDhttp') {
+      if (httpHome.pending.length) {
         log('pull', { uuid })
         httpHome.pull(uuid)
       } else {
-        const json = parse(message)
-        if (typeof json === 'object') {
-          const { key, data, query } = json
-          if (data) {
-            if (resolveTable.has(key)) {
-              resolveTable.get(key)(data)
-              resolveTable.delete(key)
-            }
-          } else if (query) {
-            const route = router[query as keyof typeof router]
-            if (route) {
-              const result = route()
-              ws.send(JSON.stringify({
-                key,
-                data: {
-                  type: 'query',
-                  result,
-                },
-              }))
-            }
+        ws.send('wait')
+      }
+    } else if (message === 'DDhttp') {
+      log('pull', { uuid })
+      httpHome.pull(uuid)
+    } else {
+      const json = parse(message)
+      if (typeof json === 'object') {
+        const { key, data, query } = json
+        if (data) {
+          if (resolveTable.has(key)) {
+            resolveTable.get(key)(data)
+            resolveTable.delete(key)
+          }
+        } else if (query) {
+          const route = router[query as keyof typeof router]
+          if (route) {
+            const result = route()
+            ws.send(JSON.stringify({
+              key,
+              data: {
+                type: 'query',
+                result,
+              },
+            }))
           }
         }
       }
-    })
-    ws.on('close', n => {
-      log('close', { n, uuid })
-      httpHome.quit(uuid)
-    })
+    }
   })
-}
+  ws.on('close', n => {
+    log('close', { n, uuid })
+    httpHome.quit(uuid)
+  })
+})
+
